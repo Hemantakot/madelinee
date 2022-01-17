3691c64a3ede99626d8f768f7736d47363716b69
Hemantakot <80027513+Hemantakot@users.noreply.github.com>
2022-01-17 17:27:33 +0530

    Create menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ const Badboy= require('../events');
 const config = require('../config');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-const hrs = new Date().getHours({ timeZone: 'Africa/Harare' })
+const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata})
 
 Badboy.addCommand({pattern: 'menu', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
 
